c6ef6dc85f20d3159900bcaed330d146132ff482
Justin Blalock <justin.m.blalock@gmail.com>
2021-09-26 17:22:37 -0500

    eitherCallback

---
+++ callbacks-and-higher-order-functions/eitherCallback.js
@@ -0,0 +1,21 @@
+function eitherCallback(callback1, callback2) {
+  // ADD CODE HERE
+  const result = function(element){
+    return callback1(element) || callback2(element);
+  }
+  return result;
+}
+
+// Uncomment these to check your work!
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i], i, array)) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [10, 35, 105, 9];
+const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+const over100 = n => n > 100;
+const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
+console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
