7e46be2d3a0302e2fac2c83e0656cb075c9c77a8
Justin Blalock <justin.m.blalock@gmail.com>
2021-10-06 17:30:11 -0500

    cycle iterator

--- callbacks-and-higher-order-functions/closure/cycleIterator.js
+++ callbacks-and-higher-order-functions/closure/cycleIterator.js
@@ -1,17 +1,10 @@
 // ADD CODE HERE
 function cycleIterator(arr) {
-  var i = 0;
-
+  var counter = -1;
   function f() {
-    i++;
-    if (i <= arr.length) return arr[i - 1];
-    else {
-      i = 0;
-      i++;
-      return arr[i - 1];
-    }
+    counter++;
+    return arr[counter];
   }
-
   return f;
 }
 // Uncomment these to check your work!
