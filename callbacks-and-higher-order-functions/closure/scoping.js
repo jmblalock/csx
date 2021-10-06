771a4029a258354b480145a45e18f2d25ecf1f45
Justin Blalock <justin.m.blalock@gmail.com>
2021-10-06 15:15:37 -0500

    scoping

---
+++ callbacks-and-higher-order-functions/closure/scoping.js
@@ -0,0 +1,21 @@
+function outer() {
+  let counter = 0; // this variable is outside incrementCounter's scope
+  function incrementCounter() {
+    counter++;
+    console.log("counter", counter);
+  }
+  return incrementCounter;
+}
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+willCounter();
+willCounter();
+willCounter();
+
+jasCounter();
+willCounter();
